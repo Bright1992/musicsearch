d0e4596cebc3a2a41fa543b05078c3faf6c1bff9
Bright <wangbowenmax@163.com>
2017-06-18 15:10:20 +0800

    2

--- musicsearch/views.py
+++ musicsearch/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import *
 from .query import query, update_info
 from django.http import HttpResponse, HttpResponseRedirect
-import copy
+import copy, os
 
 # Create your views here.
 
@@ -66,7 +66,8 @@ def show_results(request):
                 for a in ctx["songs"][-1]["artists"]:
                     try:  # artists with same name is not considered..
                         a["id"] = song_obj.artists.filter(
-                            name=a["name"].replace("<font color=\"#FF0000\">", "").replace("</font>", "")).first().artist_id
+                            name=a["name"].replace("<font color=\"#FF0000\">", "").replace("</font>",
+                                                                                           "")).first().artist_id
                         # print(a)
                         if a["id"] == 0:  # due to bugs in models...
                             ctx["songs"][-1]["artists"].remove(a)
@@ -92,7 +93,7 @@ def show_results(request):
             artist_id = int(artist_id)
             ctx["songs"] = []
             i = 0
-            songs=[]
+            songs = []
             for s in Artist.objects.get(artist_id=artist_id).song_set.all():
                 i = i + 1
                 songs.append({
@@ -114,7 +115,7 @@ def show_results(request):
                     "publish_time": s.album.publish_time,
                     "clicked_num": s.clicked_num,
                     "listened_num": s.listened_num,
-                    "popularity":s.popularity,
+                    "popularity": s.popularity,
                 })
                 for a in songs[-1]["artists"]:
                     if a["id"] == 0:
@@ -122,10 +123,10 @@ def show_results(request):
                 songs[-1]["artists"] = songs[-1]["artists"][:-1]
             songs.sort(key=lambda song: song["popularity"] + beta * (
                 song["clicked_num"] + alpha * song["listened_num"]), reverse=True)
-            i=0
+            i = 0
             for s in songs:
-                i+=1
-                s["num"]=i
+                i += 1
+                s["num"] = i
             ctx["songs"] = songs
 
             # except Exception as e:
@@ -156,22 +157,22 @@ def show_results(request):
                                 "id": s.album.album_id,
                                 "name": s.album.name
                             },
-                            "publish_time":Album.objects.get(album_id=album_id).publish_time,
-                            "clicked_num":s.clicked_num,
-                            "listened_num":s.listened_num,
-                            "popularity":s.popularity,
+                            "publish_time": Album.objects.get(album_id=album_id).publish_time,
+                            "clicked_num": s.clicked_num,
+                            "listened_num": s.listened_num,
+                            "popularity": s.popularity,
                         })
                         for a in songs[-1]["artists"]:
                             if a["id"] == 0:
                                 songs[-1]["artists"].remove(a)
                         songs[-1]["artists"] = songs[-1]["artists"][:-1]
                     songs.sort(key=lambda song: song["popularity"] + beta * (
-                        song["clicked_num"] + alpha * song["listened_num"]),reverse=True)
+                        song["clicked_num"] + alpha * song["listened_num"]), reverse=True)
                     ctx["songs"] = songs
-                    i=0
+                    i = 0
                     for s in songs:
-                        i+=1
-                        s["num"]=i
+                        i += 1
+                        s["num"] = i
                 except Exception as e:
                     print(e)
     return HttpResponse(render(request, "results.html", context=ctx))
@@ -200,6 +201,10 @@ def show_details(request):
         ctx['click_num'] = song_obj.clicked_num
         ctx['download_num'] = song_obj.listened_num
         ctx['popularity'] = song_obj.popularity
+        BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        print(BASE_DIR)
+        with open(os.path.join(BASE_DIR,"..","data","lyrics","{}.lrc".format(id)),'r') as f:
+            ctx['lrc'] = f.readlines()
     except Exception as e:
         print(e)
         return HttpResponseRedirect("error.html")
